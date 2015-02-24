7919d79b4f2b182ffd1646ce44fc9d2720c716be
Rainer Jung <rainer.jung@gmail.com>
2015-02-24 19:45:27 +0100

    Minimal implementation

--- web/src/main/java/org/rjung/alfred/Service.java
+++ web/src/main/java/org/rjung/alfred/Service.java
@@ -22,18 +22,4 @@ public class Service {
 		SpringApplication.run(Service.class, args);
 	}
 
-	@Bean
-	public LdapContextSource contextSource() {
-		LdapContextSource contextSource = new LdapContextSource();
-		contextSource.setUrl(env.getRequiredProperty("ldap.url"));
-		contextSource.setBase(env.getRequiredProperty("ldap.base"));
-		contextSource.setUserDn(env.getRequiredProperty("ldap.user"));
-		contextSource.setPassword(env.getRequiredProperty("ldap.password"));
-		return contextSource;
-	}
-
-	@Bean
-	public LdapTemplate ldapTemplate() {
-		return new LdapTemplate(contextSource());
-	}
 }
